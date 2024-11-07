d9f0dd3fffeb925a2b8485e851565e3e78406549
L3yth0n-H1d4lg0 <leythonhidalgo22@gmail.com>
2024-11-07 11:23:35 -0500

    Proyecto Maven de inventarios implementando los paquetes, clases y métodos necesarios para las conexiones con las tablas de la BDD proveedores y tipo_documentos

---
+++ inventarios/src/main/java/com/krakedev/inventario/servicios/ServiciosTipoDocumentos.java
@@ -0,0 +1,34 @@
+package com.krakedev.inventario.servicios;
+
+import java.util.ArrayList;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+import com.krakedev.inventarios.bdd.TipoDocumentosBDD;
+import com.krakedev.inventarios.entidades.TipoDocumento;
+import com.krakedev.inventarios.excepciones.KrakeDevException;
+
+@Path("tiposdocumento")
+public class ServiciosTipoDocumentos {
+
+	@Path("recuperar")
+	@GET
+	@Produces(MediaType.APPLICATION_JSON)
+	public Response probarRecuperar() {
+		TipoDocumentosBDD tdbdd = new TipoDocumentosBDD();
+		ArrayList<TipoDocumento> tipoDocumentos = null;
+
+		try {
+			tipoDocumentos = tdbdd.recuperarTodos();
+			return Response.ok(tipoDocumentos).build();
+		} catch (KrakeDevException e) {
+			e.printStackTrace();
+			return Response.serverError().build();
+		}
+	}
+
+}
